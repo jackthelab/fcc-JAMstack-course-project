539a52f112968ce49a13e1e003d446c33191a4b6
Jacob Hampton <jhampton792@gmail.com>
2021-11-16 22:34:53 -0600

    changes for localization

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,17 +1,40 @@
 const axios = require('axios');
 require('dotenv').config();
+const countries = require("./countries.json");
 
-const topNewsReqURL = `${process.env.BASE_URL_NEWS}?country=us${process.env.API_KEY_NEWS}&pageSize=5`;
+const getHeadlines = async (country) => {
 
-const getHeadlines = async () => {
+  const topNewsReqURL = `${process.env.BASE_URL_NEWS}?country=${country}${process.env.API_KEY_NEWS}&pageSize=3`;
+
   try {
+
     const response = await axios.get(topNewsReqURL);
-    console.log(response.data);
-    return response.data;
+    console.log('This is individual response data: ', response.data.totalResults);
+    return {
+      "country": country,
+      "articles": response.data.articles
+    };
+
   } catch (e) {
+
     console.error(e);
+
   }
 
-}
+};
 
-module.exports = getHeadlines;
+
+
+module.exports = async function() {
+  
+  const newsHeadlines = countries.map(
+    country => getHeadlines(country)
+  );
+
+  // getHeadlines is returning promises
+  return Promise.all(newsHeadlines).then( newsObjects => {
+    console.log('newsObjects: ', newsObjects);
+    return [].concat.apply([], newsObjects);
+  })
+  
+}
